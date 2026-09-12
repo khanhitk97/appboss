8f3d9aa9c682bc37182473de2753f2863ec7d5ef
khanhitk97 <vdkhanh.22011997@gmail.com>
2026-09-12 13:00:47 +0700

    Create Speedhack.m

---
+++ Speedhack.m
@@ -0,0 +1,136 @@
+#import <Foundation/Foundation.h>
+#import <sys/time.h>
+#import <CoreFoundation/CoreFoundation.h>
+#import <objc/runtime.h>
+#import <mach/mach_time.h>
+#import <os/lock.h>
+#import "fishhook.h"
+
+static float speed_factor = 5.0f;
+static os_unfair_lock speed_lock = OS_UNFAIR_LOCK_INIT;
+
+static int (*orig_gettimeofday)(struct timeval *tv, struct timezone *tz) = NULL;
+static CFAbsoluteTime (*orig_CFAbsoluteTimeGetCurrent)(void) = NULL;
+static uint64_t (*orig_mach_absolute_time)(void) = NULL;
+
+static struct timeval last_real_tv = {0, 0}, fake_tv = {0, 0};
+static CFAbsoluteTime last_real_cf = 0, fake_cf = 0;
+static uint64_t last_real_mach = 0, fake_mach = 0;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void set_speed_factor(float factor) {
+    os_unfair_lock_lock(&speed_lock);
+    speed_factor = factor;
+    
+    if (factor == 1.0f) {
+        last_real_tv = (struct timeval){0, 0};
+        fake_tv = (struct timeval){0, 0};
+        last_real_cf = 0;
+        fake_cf = 0;
+        last_real_mach = 0;
+        fake_mach = 0;
+    }
+    os_unfair_lock_unlock(&speed_lock);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+int my_gettimeofday(struct timeval *tv, struct timezone *tz) {
+    int ret = orig_gettimeofday(tv, tz);
+    if (ret != 0 || !tv) return ret;
+
+    os_unfair_lock_lock(&speed_lock);
+    if (speed_factor == 1.0f) {
+        os_unfair_lock_unlock(&speed_lock);
+        return ret;
+    }
+
+    if (last_real_tv.tv_sec == 0) {
+        last_real_tv = *tv;
+        fake_tv = *tv;
+    } else {
+        double delta = (tv->tv_sec - last_real_tv.tv_sec) + (tv->tv_usec - last_real_tv.tv_usec) / 1e6;
+        double fake_delta = delta * speed_factor;
+        long sec_add = (long)fake_delta;
+        long usec_add = (long)((fake_delta - sec_add) * 1e6);
+
+        fake_tv.tv_sec += sec_add;
+        fake_tv.tv_usec += usec_add;
+        if (fake_tv.tv_usec >= 1000000) {
+            fake_tv.tv_sec += 1;
+            fake_tv.tv_usec -= 1000000;
+        }
+        last_real_tv = *tv;
+    }
+    *tv = fake_tv;
+    os_unfair_lock_unlock(&speed_lock);
+    return ret;
+}
+
+CFAbsoluteTime my_CFAbsoluteTimeGetCurrent(void) {
+    CFAbsoluteTime real_now = orig_CFAbsoluteTimeGetCurrent();
+
+    os_unfair_lock_lock(&speed_lock);
+    if (speed_factor == 1.0f) {
+        os_unfair_lock_unlock(&speed_lock);
+        return real_now;
+    }
+
+    if (last_real_cf == 0) {
+        last_real_cf = real_now;
+        fake_cf = real_now;
+    } else {
+        fake_cf += (real_now - last_real_cf) * speed_factor;
+        last_real_cf = real_now;
+    }
+    CFAbsoluteTime result = fake_cf;
+    os_unfair_lock_unlock(&speed_lock);
+    return result;
+}
+
+uint64_t my_mach_absolute_time(void) {
+    uint64_t real_now = orig_mach_absolute_time();
+
+    os_unfair_lock_lock(&speed_lock);
+    if (speed_factor == 1.0f) {
+        os_unfair_lock_unlock(&speed_lock);
+        return real_now;
+    }
+
+    if (last_real_mach == 0) {
+        last_real_mach = real_now;
+        fake_mach = real_now;
+    } else {
+        fake_mach += (uint64_t)((real_now - last_real_mach) * speed_factor);
+        last_real_mach = real_now;
+    }
+    uint64_t result = fake_mach;
+    os_unfair_lock_unlock(&speed_lock);
+    return result;
+}
+
+__attribute__((constructor))
+static void initialize(void) {
+    struct rebinding rebindings[] = {
+        {"gettimeofday", (void *)my_gettimeofday, (void **)&orig_gettimeofday},
+        {"CFAbsoluteTimeGetCurrent", (void *)my_CFAbsoluteTimeGetCurrent, (void **)&orig_CFAbsoluteTimeGetCurrent},
+        {"mach_absolute_time", (void *)my_mach_absolute_time, (void **)&orig_mach_absolute_time}
+    };
+    rebind_symbols(rebindings, 3);
+
+    Class nsdateClass = [NSDate class];
+    Method origRef = class_getClassMethod(nsdateClass, @selector(timeIntervalSinceReferenceDate));
+    if (origRef) method_setImplementation(origRef, (IMP)my_CFAbsoluteTimeGetCurrent);
+    
+    Method origDate = class_getClassMethod(nsdateClass, @selector(date));
+    if (origDate) {
+        method_setImplementation(origDate, imp_implementationWithBlock(^id(id self) {
+            return [NSDate dateWithTimeIntervalSinceReferenceDate:my_CFAbsoluteTimeGetCurrent()];
+        }));
+    }
+}
